0a6f61a245ac2610b9f3b522943f43daccf6f7df
JosePPalFer <palomarfernandez20@gmail.com>
2025-09-23 10:53:53 +0200

    Cambios echo

--- miPrimeraPrueba.php
+++ miPrimeraPrueba.php
@@ -10,6 +10,9 @@
 echo "My first PHP script!"; // Las cadenas de texto van estre comillas.
 //todas las sentencias de PHP deben terminar en punto y coma.
 
+$edad = 25;
+$nombre = "Jose Pablo";
+
 // Fin de código PHP
 ?>
 
