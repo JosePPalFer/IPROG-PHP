f6298cbf61d397cb704f459c66787ff9b2c0f164
JosePPalFer <palomarfernandez20@gmail.com>
2025-09-25 09:40:53 +0200

    Clase Septiembre 25

--- miPrimeraPrueba.php
+++ miPrimeraPrueba.php
@@ -16,5 +16,30 @@ $nombre = "Jose Pablo";
 // Fin de código PHP
 ?>
 
+echo
+    >?
+    <h1>
+    <?php $nombre
+    ?> </h1>
+    <?php ?>;
+
+<?php
+$entero = 5;
+$decimal = 10.5;
+
+$x = true;
+echo "El tipo de dato de la variable \$x es:"
+.var_dump($x);
+$tipoDato = "var_dump($x)";
+
+echo "<br>El tipo de dato de la variable \$x es: $tipoDato";
+
+echo "<br>Este es un "; //Imprime un salto de línea
+echo "<br>Aquí acaba el ejemplo"; //Imprime un salto de línea en HTML
+
+$cars = array("Volvo","BMW","Toyota");//Array guarda multiples variables en una variable
+var_dump($cars);
+echo "Mi coche es un:". $cars[2];
+?>
 </body>
 </html>
