aaddb98163ce55897438ff14b1f55ebbaa0d3fbe
JosePPalFer <palomarfernandez20@gmail.com>
2025-09-25 09:50:59 +0200

    Clase 25 Septiembre

--- PracticasPHP.php
+++ PracticasPHP.php
@@ -2,8 +2,11 @@
 <html>
 <body>
 <?php
-$alumno = array("José Pablo","Palomar","Fernández",27, 8.8, );
+$alumno = array("José Pablo","Palomar","Fernández",27, 8.8, ); //Practica 1
 echo "El alumno".$alumno(0) .$alumno(1), "ha aprobado con" .$alumno(3),"años";
+
+$compra = array ("Melocotón","6","4");
+echo "La".$compra(0), "esta a" .$alumno(1), "€ el Kg";
 ?>
 </body>
 </html>
