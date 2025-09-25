b63708a68903da8aaeec4fa7a509020c3bdffc5b
JosePPalFer <palomarfernandez20@gmail.com>
2025-09-25 09:42:10 +0200

    Clase 25

---
+++ PracticasPHP.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$alumno = array("José Pablo","Palomar","Fernández",27, 8.8, );
+echo "El alumno".$alumno(0) .$alumno(1), "ha aprobado con" .$alumno(3),"años";
+?>
+</body>
+</html>
