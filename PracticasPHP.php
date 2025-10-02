0d96c544e290ea723495da172f7c522085621ded
JosePPalFer <palomarfernandez20@gmail.com>
2025-10-02 13:43:54 +0200

    Clase 02 Octubre

--- PracticasPHP.php
+++ PracticasPHP.php
@@ -6,8 +6,8 @@ $alumno = array("José Pablo","Palomar","Fernández",27, 8.8); //Practica 1
 echo "El alumno ".$alumno[0]," ", $alumno[1], " ha aprobado con " .$alumno[3]," años<br>";
 
 $compra = array ("Melocotón","6","4"); //Práctica 2
-echo "<br>El ".$compra[0], "esta a " .$alumno[1], " € el Kg<br>";
-echo "<br>La compra me ha salido a " .$alumno[1];
+echo "<br>El ".$compra[0], " esta a " .$compra[1], " € el Kg<br>";
+echo "<br>La compra me ha salido a " .$compra[1] * 3, " €";
 ?>
 </body>
 </html>
