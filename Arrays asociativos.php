30f90f879b8d947c85cbcdc44d883ca57caf8eaa
JosePPalFer <palomarfernandez20@gmail.com>
2025-11-13 13:45:43 +0100

    Clase 13 Noviemblre

---
+++ Arrays asociativos.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+//1. Practica: Crear array que contemple Nombre, Apellido, Modulos con sus notas.
+// Calcular la media de las notas.
+$alumno1 = array("Nombre" => "Pedro",
+    "Apellido" => "Perez",
+    "ASGBD" => 6,
+    "PHP" => 2,
+    "IMWEB" => 7,
+    "SRI" => 8);
+
+$alumno2 = array("Nombre" => "Miguel",
+    "Apellido" => "Campos",
+    "ASGBD" => 5,
+    "PHP" => 4,
+    "IMWEB" => 6,
+    "SRI" => 3);
+
+$notasalumno1 = $alumno1["ASGBD"] + $alumno1["PHP"] + $alumno1["IMWEB"] + $alumno1["SRI"];
+
+$notasalumno2 = $alumno2["ASGBD"] + $alumno2["PHP"] + $alumno2["IMWEB"] + $alumno2["SRI"];
+
+echo "Notas de " . $alumno1["Nombre"] . " " . $alumno1["Apellido"] . ": " . $notasalumno1 / 4 . "<br>";
+
+echo "Notas de " . $alumno2["Nombre"] . " " . $alumno2["Apellido"] . ": " . $notasalumno2 / 4 . "<br><br>";
+
+foreach ($alumno1 as $etiqueta => $valor) {
+    if($etiqueta == "ASGBD" or $etiqueta == "PHP"){
+        echo "Mi nota con Ángela en: " . $etiqueta . " es: " .$valor . "<br>";
+    }
+}
+
+foreach ($alumno1 as $etiqueta => $valor) {
+    if (is_numeric($valor) && $valor >= 5) {
+        echo "<br>Mi nota en " . $etiqueta . " esta aprobada.<br>";
+    }
+}
+
+//Ejemplo con zapatillas
+$zapatos = array("Marca" => "Nike",
+    "Color" => "Azul",
+    "Precio" => 48,
+    "Stock" => 9);
+
+foreach ($zapatos as $x => $y) {
+    echo "<br>" . $x . ": " . $y;
+}
+?>
+</body>
+</html>
