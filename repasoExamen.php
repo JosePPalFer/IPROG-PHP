66eb1edeb8eb7732c2a23d770649b2b7d674f264
JosePPalFer <palomarfernandez20@gmail.com>
2025-12-11 11:08:55 +0100

    Clase 11 Diciembre

---
+++ repasoExamen.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+// Genera un array para almacenar información de empresas, con la siguiente estructura
+
+// empresas
+// Nombre: Nombre de la empresa
+// CIF/NIF: X12345678
+// Teléfono: 61234567
+// Localidad: Écija
+// Facturación: 3000€
+
+echo "0.- Une todos los arrays anteriores";
+
+
+echo "1.- A partir del paso anterior, imprime toda la información de la empresa con el siguiente formato: La empresa 
+    [nombre empresa] con CIF [cif], tiene sede en [Localidad] y su facturación es de [facturación].";
+
+echo "2.- Crea una variable BuscarFacturación que almacene el valor medio de la facturación
+    de las 5 empresas.";
+
+echo "3.- Imprime las empresas que tienen una facturación mayor a la media.";
+
+echo "4.- Introduce los datos de la siguiente empresa en tu array.";
+
+echo "5.- Busca las empresas cuyo nombre contenga PHP.";
+
+echo "6.- Elimina la empresa con menor facturación y muestra como queda el array";
+
+echo "7.- ";
+
+?>
+</body>
+</html>
