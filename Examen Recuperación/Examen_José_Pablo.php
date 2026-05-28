431c13c816ea4e9c92d424c1bf83df38fc77f815
JosePPalFer <palomarfernandez20@gmail.com>
2026-05-28 13:43:51 +0200

    Clase 28 Mayo

--- Examen Recuperación/Examen_José_Pablo.php
+++ Examen Recuperación/Examen_José_Pablo.php
@@ -1,29 +1,143 @@
 <?php
-$EventosAndalucia = array("E01" => "Evento: Weekend Beach, Aforo: 40, Provincia: Málaga",
-                        "E02" => "Eve nto: Dreambeach, Aforo: 25, ProvinciA: Almeria",
-                        "E03" => "Evento: Monkey Week, Aforo: 5, Provincia: Sevilla",
-                        "E04" => "EventO: Cabo de Plata, Aforo: 30, Provincia: Cádiz",
-                        "E05" => "evento: Granada Sound, Aforo: 20, Provincia: Granada");
+$EventosAndalucia = [
+'E01' => 'Evento: Weekend Beach, Aforo: 40, Provincia: Málaga',
+'E02' => 'Eve nto: Dreambeach, Aforo: 25, ProvinciA: Almeria',
+'E03' => 'Evento: Monkey Week, Aforo: 5, Provincia: Sevilla',
+'E04' => 'EventO: Cabo de Plata, Aforo: 30, Provincia: Cádiz',
+'E05' => 'evento: Granada Sound, Aforo: 20, Provincia: Granada'
+];
 
-$EventosNacionales = array("G10" => "DreamHack, Aforo: 60, Lugar: Valencia",
-                    "G11" => "Evento: Gamepolis, Aforo: 50, lugar: Malaga");
+$EventosNacionales = [
+'G10' => 'Evento: DreamHack, Aforo: 60, Lugar: Valencia',
+'G11' => 'Evento: Gamepolis, Aforo: 50, lugar: Malaga'
+];
+
+function ordenar_andalucia() {
+    $EventosAndalucia = [
+'E01' => 'Evento: Weekend Beach, Aforo: 40, Provincia: Málaga',
+'E02' => 'Eve nto: Dreambeach, Aforo: 25, ProvinciA: Almeria',
+'E03' => 'Evento: Monkey Week, Aforo: 5, Provincia: Sevilla',
+'E04' => 'EventO: Cabo de Plata, Aforo: 30, Provincia: Cádiz',
+'E05' => 'evento: Granada Sound, Aforo: 20, Provincia: Granada'
+];
+
+foreach ($EventosAndalucia as $etiqueta => $valor)
+    {
+    echo "" . $etiqueta . " => " . $valor;
+    echo "<br>";
+    }
+}
+
+ordenar_andalucia();
+
+echo "<br>";
+
+function ordenar_nacional() {
+    $EventosNacionales = [
+'G10' => 'Evento: DreamHack, Aforo: 60, Lugar: Valencia',
+'G11' => 'Evento: Gamepolis, Aforo: 50, lugar: Malaga'
+];
+
+foreach ($EventosNacionales as $etiqueta => $valor)
+    {
+    echo "" . $etiqueta . " => " . $valor;
+    echo "<br>";
+    }
+}
+
+ordenar_nacional();
+
+echo "<br>";
 
 // 1: Unión y listado
 $AgendaTotal = array_merge($EventosAndalucia, $EventosNacionales);
 
-echo $AgendaTotal;
+function ordenar_total() {
+    $AgendaTotal = [
+'E01' => 'Evento: Weekend Beach, Aforo: 40, Provincia: Málaga',
+'E02' => 'Eve nto: Dreambeach, Aforo: 25, ProvinciA: Almeria',
+'E03' => 'Evento: Monkey Week, Aforo: 5, Provincia: Sevilla',
+'E04' => 'EventO: Cabo de Plata, Aforo: 30, Provincia: Cádiz',
+'E05' => 'evento: Granada Sound, Aforo: 20, Provincia: Granada',
+'G10' => 'Evento: DreamHack, Aforo: 60, Lugar: Valencia',
+'G11' => 'Evento: Gamepolis, Aforo: 50, lugar: Malaga'
+];
+
+foreach ($AgendaTotal as $etiqueta => $valor)
+    {
+    echo "" . $etiqueta . " => " . $valor;
+    echo "<br>";
+    }
+}
+
+ordenar_total();
+
+echo "<br>";
 
 // 2. Tratamiento de Cadenas y Cálculo
-$AforoMalaga = explode("Málaga", "$AgendaTotal");
+echo stripos("Evento: Weekend Beach, Aforo: 40, Provincia: Málaga","Aforo");
 
-if (strpos("$AforoMalaga", "Aforo: "));
+echo "<br>";
 
+$str="Evento: Weekend Beach, Aforo: 40, Provincia: Málaga";
+print_r(explode(",",$str));
+
+echo "<br>";
+echo "<br>";
 
 // 3. Modificación del array
-$AgendaTotal <= array("P01" => "Evento: Puro Latino, Aforo: 35, Provincia: Cádiz");
+$AgendaTotal["P01"] = "Evento: Puro Latino, Aforo: 35, Provincia: Cadiz";
 
-echo $AgendaTotal;
+function ordenar_total3() {
+    $AgendaTotal = [
+'E01' => 'Evento: Weekend Beach, Aforo: 40, Provincia: Málaga',
+'E02' => 'Eve nto: Dreambeach, Aforo: 25, ProvinciA: Almeria',
+'E03' => 'Evento: Monkey Week, Aforo: 5, Provincia: Sevilla',
+'E04' => 'EventO: Cabo de Plata, Aforo: 30, Provincia: Cádiz',
+'E05' => 'evento: Granada Sound, Aforo: 20, Provincia: Granada',
+'G10' => 'Evento: DreamHack, Aforo: 60, Lugar: Valencia',
+'G11' => 'Evento: Gamepolis, Aforo: 50, lugar: Malaga',
+'P01' => 'Evento: Puro Latino, Aforo: 35, Provincia: Cadiz'
+];
+
+foreach ($AgendaTotal as $etiqueta => $valor)
+    {
+    echo "" . $etiqueta . " => " . $valor;
+    echo "<br>";
+    }
+}
+
+ordenar_total3();
+
+echo "<br>";
 
 // 4. Eliminación con Condicionales
 
+
+// 5. Control de Estados / Umbrales
+
+
+// 6. Ordenación
+function ordenar_total6() {
+    $AgendaTotal = [
+'E01' => 'Evento: Weekend Beach, Aforo: 40, Provincia: Málaga',
+'E02' => 'Eve nto: Dreambeach, Aforo: 25, ProvinciA: Almeria',
+'E03' => 'Evento: Monkey Week, Aforo: 5, Provincia: Sevilla',
+'E04' => 'EventO: Cabo de Plata, Aforo: 30, Provincia: Cádiz',
+'E05' => 'evento: Granada Sound, Aforo: 20, Provincia: Granada',
+'G10' => 'Evento: DreamHack, Aforo: 60, Lugar: Valencia',
+'G11' => 'Evento: Gamepolis, Aforo: 50, lugar: Malaga',
+'P01' => 'Evento: Puro Latino, Aforo: 35, Provincia: Cadiz'
+];
+
+ksort($AgendaTotal);
+
+foreach ($AgendaTotal as $etiqueta => $valor)
+    {
+    echo "" . $etiqueta . " => " . $valor;
+    echo "<br>";
+    }
+}
+
+ordenar_total6();
 ?>
